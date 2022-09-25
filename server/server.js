fc88e060997ab8afc1b1c132877873b2aa008a79
Channchealy Sek <sek.channchealy@yahoo.com>
2022-09-25 17:47:23 -0500

    create server side

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// to use the client folder in server/server.js.
+app.use(express.static('../client/'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Require the HTML route in the server.js file by using
+require('./routes/htmlRoutes.js')(app);
+
+app.listen(PORT, function() {
+  console.log(`Now listening on port: ${PORT}`);
+});
